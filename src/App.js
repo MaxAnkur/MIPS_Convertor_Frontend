803a8c9ea372b2a732e65d8d718251635eb919fa
Ankur P Jha <ankurjha2009@gmail.com>
2022-02-28 22:55:16 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Analytics from './components/index'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          
+          <h3 style={{ textAlign: "center" }}>MIPS Instruction Converter</h3>
+          
+    
+      </header>
+      <Analytics />
+      <footer className="footer">
+        <div> 
+       
+        <span style={{paddingLeft:"5px"}}> Disclaimer: It is a group project done by 4 student of CSU Ohio. They are Abhinav, Abhijeet, Ankur and Lokaranjan. </span>  
+        <span style={{paddingLeft:"5px"}}> ©️ copyright 2020 </span>  
+        
+                </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
